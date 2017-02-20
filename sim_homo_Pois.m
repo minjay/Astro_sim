5b7ad500ec4ac1d6071a8aa0ef83ac2b84c66f69
minjay <mjfan@ucar.edu>
2017-02-20 20:29:08 +0800

    Add comments and add a new variable seed.

--- sim_homo_Pois.m
+++ sim_homo_Pois.m
@@ -1,8 +1,23 @@
-function X = sim_homo_Pois(range_x, range_y, lambda)
+function X = sim_homo_Pois(range_x, range_y, lambda, seed)
+% simulate homogeneous Poisson process
+% Input variables:
+%
+% range_x: the range of x
+% range_y: the range of y
+% lambda: density of the process (which is a constant)
+% seed: random seed
+%
+% Output variables:
+% X: an n-by-2 matrix, where each row represents the location of one of the 
+% points
 
 len_x = range_x(2)-range_x(1);
 len_y = range_y(2)-range_y(1);
+rng(seed)
+% n represents the number of points, which follows a Poisson distribution
 n = poissrnd(len_x*len_y*lambda);
+% X is an n-by-2 matrix, where each row represents the location of one of
+% the points
 X = zeros(n, 2);
 X(:, 1) = rand(n, 1)*len_x+range_x(1);
 X(:, 2) = rand(n, 1)*len_y+range_y(1);
