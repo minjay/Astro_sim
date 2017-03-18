a7baa73dd79feed3db1980e2ef9ee6c2bc588548
minjay <mjfan@ucar.edu>
2017-03-18 16:10:48 -0700

    Make main1 work for repetitions.

---
+++ main1_rep.m
@@ -0,0 +1,37 @@
+% numerical experiment 1
+% one round-shaped extended source
+
+clear
+close all
+
+rng(1)
+T = 10;
+n_source = zeros(T, 1);
+
+for t = 1:T
+    disp(['The ', num2str(t), '-th repetition'])
+    % generate simulated data (inhomogeneous Poisson point process)
+    X = sim_inhomo_Pois_const([0 1], [0 1], 1000, [0.5 0.5], 0.25, 200);
+
+    % init comp
+    [cx, cy, n, DT, E, cell_log_intensity, cell_area] = init_comp(X, [0 1], [0 1]);
+
+    % get seeds
+    [seeds, num, invalid] = get_seeds_sim(0.1, 0.9, 0.1, 0.9,...
+        0.2, 0.2, 5, cell_log_intensity, cell_area, cx, cy, 2);
+    disp(['Number of regions is ', num2str(num)])
+
+    % make a copy of variable seeds
+    region_sets = seeds;
+
+    % graph-based SRG
+    adj_mat = get_adj_mat( E, n );
+    [region_sets, labeled_cells] = SRG_graph(region_sets, cell_log_intensity, cell_area, n, adj_mat, invalid');
+
+    [sets_all, log_like_all] = merge_region(num, cell_area, ...
+        cell_log_intensity, region_sets, adj_mat, n);
+
+    BIC_all = -2*log_like_all+6*(num-1:-1:0)'*log(n);
+    [~, index_BIC] = min(BIC_all);
+    n_source(t) = num-index_BIC;
+end
