d8036f8d9a95d12b78900050428c199d0b73bb8a
minjay <mjfan@ucar.edu>
2017-03-21 09:44:18 -0700

    Correct a mistake. Now use distance to match.

--- perf_eval.m
+++ perf_eval.m
@@ -1,5 +1,5 @@
-function [dr, far, err, res_source_area_sort, source_x, source_y, n_S_correct] =...
-    perf_eval(n_source, loc, radius, num_of_photon, cx, cy, selected, cell_log_intensity, cell_area)
+function [dr, far, err, region_area, source_x, source_y, n_S_correct] =...
+    perf_eval(n_source, loc, radius, cx, cy, selected, cell_log_intensity, cell_area)
 % performance evaluation
 
 % compute/estimate the intensity of the fitted regions
@@ -9,21 +9,10 @@ for i = 1:n_source+1
     region_area(i) = sum(cell_area(selected{i}));
     region_intensity(i) = sum(cell_area(selected{i}).*exp(cell_log_intensity(selected{i})))/region_area(i);
 end
-% sort regions by their intensities
-[~, res_index_sort] = sort(region_intensity, 'descend');
-% obtain the fitted source sets, sorted in descending order of
-% intensities
-res_source_sort = selected(res_index_sort(1:end-1));
-res_source_area_sort = region_area(res_index_sort(1:end-1));
-% compute/estimate the intensity of the true sources
-% only works for round-shaped sources
-est_source_int = num_of_photon./(pi*radius.^2);
-% sort sources by their intensities
-[~, data_index_sort] = sort(est_source_int, 'descend');
-% obtain the true location, radius of the sources, sorted in descending 
-% order of intensities
-loc_sort = loc(data_index_sort, :);
-radius_sort = radius(data_index_sort);
+[background_intensity, index_background] = min(region_intensity);
+% remove background
+selected(index_background) = [];
+region_area(index_background) = [];
 % n1 records the number of successfully detected source photons for
 % each source
 % n2 records the number of false alarms for each source
@@ -38,25 +27,45 @@ n_Sc = zeros(n_source, 1);
 source_x = zeros(n_source, 1);
 source_y = zeros(n_source, 1);
 n_S_correct = zeros(n_source, 1);
-for i = 1:n_source
-    dist_sq = (cx(res_source_sort{i})-loc_sort(i, 1)).^2+(cy(res_source_sort{i})-loc_sort(i, 2)).^2;
-    n1(i) = length(find(dist_sq<=radius_sort(i)^2));
-    n2(i) = length(find(dist_sq>radius_sort(i)^2));
-    dist_sq = (cx-loc_sort(i, 1)).^2+(cy-loc_sort(i, 2)).^2;
-    n_S(i) = length(find(dist_sq<=radius_sort(i)^2));
-    n_Sc(i) = length(find(dist_sq>radius_sort(i)^2));
-    denom = sum(exp(cell_log_intensity(res_source_sort{i})));
-    source_x(i) = sum(exp(cell_log_intensity(res_source_sort{i})).*cx(res_source_sort{i}))/denom;
-    source_y(i) = sum(exp(cell_log_intensity(res_source_sort{i})).*cy(res_source_sort{i}))/denom;
+% note that there might be a mismatch between estimated and true sources
+for i = 1:n_source 
+    dist_sq = (cx-loc(i, 1)).^2+(cy-loc(i, 2)).^2;
+    n_S(i) = length(find(dist_sq<=radius(i)^2));
+    n_Sc(i) = length(find(dist_sq>radius(i)^2));
+    denom = sum(exp(cell_log_intensity(selected{i})));
+    source_x(i) = sum(exp(cell_log_intensity(selected{i})).*cx(selected{i}))/denom;
+    source_y(i) = sum(exp(cell_log_intensity(selected{i})).*cy(selected{i}))/denom;
     % with background correction
     % min(region_intensity) is supposed to be the intensity of the
     % background
-    n_S_correct(i) = length(res_source_sort{i})-min(region_intensity)*res_source_area_sort(i);
+    n_S_correct(i) = length(selected{i})-background_intensity*region_area(i);
 end
+index_set = 1:n_source;
+% record how estimated and true sources match with each other
+match = zeros(n_source, 1);
+for i = 1:n_source
+    % for each estimated source, pick the closest true source to match
+    dist_sq = (source_x(i)-loc(index_set, 1)).^2+(source_y(i)-loc(index_set, 2)).^2;
+    [~, index_tmp] = min(dist_sq);
+    % the index of matched true source
+    index_true_source = index_set(index_tmp);
+    % delete it from index_set
+    index_set(index_tmp) = [];
+    % record it
+    match(i) = index_true_source;
+end
+
+% compute n1 and n2 after matching
+for i = 1:n_source
+    dist_sq = (cx(selected{i})-loc(match(i), 1)).^2+(cy(selected{i})-loc(match(i), 2)).^2;
+    n1(i) = length(find(dist_sq<=radius(match(i))^2));
+    n2(i) = length(find(dist_sq>radius(match(i))^2));
+end
+  
 % detection rate
-dr = n1./n_S;
+dr = n1./n_S(match);
 % false alarm rate
-far = n2./n_Sc;
+far = n2./n_Sc(match);
 % total misclassification rate
 err = 1-dr+far;
    
