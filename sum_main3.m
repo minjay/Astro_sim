bdf72b261ec2f11f83448f4ed7a8e89c90eed519
minjay <mjfan@ucar.edu>
2017-04-21 09:04:05 -0700

    Summarize results of main3.

---
+++ sum_main3.m
@@ -0,0 +1,14 @@
+load('main3_rep_lambda_1000_num_100.mat')
+tabulate(n_source)
+
+load('main3_rep_lambda_1000_num_200.mat')
+tabulate(n_source)
+
+load('main3_rep_lambda_1000_num_300.mat')
+tabulate(n_source)
+
+load('main3_rep_lambda_2000_num_200.mat')
+tabulate(n_source)
+
+load('main3_rep_lambda_3000_num_300.mat')
+tabulate(n_source)
