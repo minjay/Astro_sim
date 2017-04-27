9a4834b7132abddd5ec333142998892c4d87a0fa
minjay <mjfan@ucar.edu>
2017-04-26 23:28:34 -0700

    Subplots.

--- main4.m
+++ main4.m
@@ -8,17 +8,24 @@ loc = [0.5 0.5; 0.4 0.4; 0.4 0.6; 0.5 0.5; 0.6 0.4; 0.6 0.6];
 radius = [0.25 0.025*ones(1, 5)];
 num = [500 50*ones(1, 5)];
 % generate simulated data (inhomogeneous Poisson point process)
-X = sim_inhomo_Pois_const([0 1], [0 1], 1000, loc, radius, num, 0, true);
+X = sim_inhomo_Pois_const([0 1], [0 1], 1000, loc, radius, num, 0);
+
+h = figure;
+
+subplot = @(m,n,p) subtightplot (m, n, p, [0.05 0.075], [0.075 0.05], [0.05 0.02]);
+subplot(2, 3, 1)
+scatter(X(:, 1), X(:, 2), '.')
+axis square
 
 % init comp
 [cx, cy, n, DT, E, cell_log_intensity, cell_area] = init_comp(X, [0 1], [0 1]);
 
 % plot log intensity
-figure
+subplot(2, 3, 2)
 triplot(DT)
 hold on
 scatter(cx, cy, [], cell_log_intensity, 'o', 'filled')
-colorbar
+%colorbar
 colormap(jet)
 axis image
 
@@ -28,7 +35,7 @@ axis image
 disp(['Number of regions is ', num2str(num)])
 
 % plot the seeds
-figure
+subplot(2, 3, 3)
 triplot(DT)
 hold on
 % specify the colormap
@@ -46,13 +53,12 @@ adj_mat = get_adj_mat( E, n );
 [region_sets, labeled_cells] = SRG_graph(region_sets, cell_log_intensity, cell_area, n, adj_mat, invalid');
 
 % plot the over-segmented image
-figure
+subplot(2, 3, 4)
 triplot(DT)
 hold on
 for i = 1:num
     scatter(cx(region_sets{i}), cy(region_sets{i}), [],  colors(i, :), 'filled')
 end
-viscircles([0.5 0.5], 0.25, 'EdgeColor', 'r', 'LineWidth', 1.5);
 axis image
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -60,18 +66,19 @@ axis image
 [sets_all, log_like_all] = merge_region(num, cell_area, ...
     cell_log_intensity, region_sets, adj_mat, n);
 
-figure
-plot(log_like_all, '-o')
+%figure
+%plot(log_like_all, '-o')
 
 BIC_all = -2*log_like_all+6*(num-1:-1:0)'*log(n);
-figure
-plot(num-1:-1:0, BIC_all, '-o')
+subplot(2, 3, 5)
+plot(num-1:-1:0, BIC_all, '-o', 'MarkerSize', 3)
 xlabel('Number of clusters/sources')
 ylabel('BIC')
+[min_BIC, index_BIC] = min(BIC_all);
+text(num-index_BIC-1, min_BIC-150, num2str(num-index_BIC), 'FontSize', 12)
+axis square
 
-[~, index_BIC] = min(BIC_all);
-
-figure
+subplot(2, 3, 6)
 triplot(DT)
 hold on
 % the final result
@@ -83,5 +90,6 @@ for i = 1:num
         scatter(cx(selected{i}), cy(selected{i}), [],  colors(index, :), 'filled')
     end
 end
-viscircles([0.5 0.5], 0.25, 'EdgeColor', 'r', 'LineWidth', 1.5);
 axis image
+
+set(h, 'Position', [0, 0, 800, 500]);
