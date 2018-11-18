5686ea55fdc2428f2c9c7595fcf71f6f0f967870
minjay <mjfan@ucar.edu>
2018-11-17 22:19:29 -0800

    Add more description and change to row vector.

--- real_img/uniform_sample_triangle.m
+++ real_img/uniform_sample_triangle.m
@@ -1,8 +1,18 @@
 function [sx, sy] = uniform_sample_triangle(vx, vy, n)
+% Sample n points uniformly in a triangle.
 % Reference: https://math.stackexchange.com/questions/18686/uniform-random-point-in-triangle
+%
+% Args:
+%   vx: A 1-by-3 vector giving the x-coordinates of the three vertices.
+%   vy: A 1-by-3 vector giving the y-coordinates of the three vertices.
+%   n: Number of points to sample.
+%
+% Returns:
+%   sx: A 1-by-n vector giving the x-coordinates of the sampled points.
+%   sy: A 1-by-n vector giving the y-coordinates of the sampled points.
 
-r1 = rand(n, 1);
-r2 = rand(n, 1);
+r1 = rand(1, n);
+r2 = rand(1, n);
 
 coef1 = 1 - sqrt(r1);
 coef2 = sqrt(r1) .* (1 - r2);
