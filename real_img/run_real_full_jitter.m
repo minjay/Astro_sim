2358aac87462e76f758a12d7eec3efc2acf12f73
minjay <mjfan@ucar.edu>
2018-11-20 21:13:30 -0800

    Change to parallel computation by parfor.

--- real_img/run_real_full_jitter.m
+++ real_img/run_real_full_jitter.m
@@ -1,6 +1,13 @@
 addpath(genpath('/home/minjay/G-SRG'))
 addpath(genpath('/home/minjay/Astro_sim'))
 
-for i = 1:10
-    real_full_jitter(i)
+load('real_full_result_2018_11_1_23_4_37.mat')
+
+n_boot = 100;
+impute_log_int = zeros(length(cx), n_boot);
+parfor i = 1:n_boot
+    impute_log_int(:, i) = real_full_jitter(i)
 end
+
+filename = 'real_full_result_boot.mat';
+save(filename, 'impute_log_int')
