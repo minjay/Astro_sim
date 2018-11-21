73a92d97bf4b4cdf70d1140542e022b28311cd48
minjay <mjfan@ucar.edu>
2018-11-20 21:22:16 -0800

    Impute log intensity of all original Voronoi cells using the estimates obtained from resampled Voronoi cells.

--- real_img/real_full_jitter.m
+++ real_img/real_full_jitter.m
@@ -1,5 +1,10 @@
-function [] = real_full_jitter(seed)
+function impute_log_int = real_full_jitter(seed)
+rng(seed)
+
 load('real_full_result_2018_11_1_23_4_37.mat')
+
+cx_origin = cx;
+cy_origin = cy;
 
 % drop empty entries in the cell array
 num = length(selected);
@@ -21,9 +26,6 @@ end
 
 num_nonempty = length(selected_nonempty);
 
-rng(seed)
-GRAY = [0.6 0.6 0.6];
-
 % jitter X
 [sx_all, sy_all, ~] = get_bootstrap_samples(cx, cy,...
     DT, num_nonempty, selected_nonempty, log_int_all);
@@ -32,19 +34,10 @@ X_jitter = [sx_all', sy_all'];
 X_jitter = unique(X_jitter, 'rows');
 n = size(X_jitter, 1);
 count = ones(n, 1);
-[cx, cy, n, DT, E, cell_log_intensity, cell_area] = init_comp(X_jitter, bound_x, bound_y, count);
+[cx, cy, n, ~, E, cell_log_intensity, cell_area] = init_comp(X_jitter, bound_x, bound_y, count);
 adj_mat = get_adj_mat( E, n );
 
-fig = figure;
-triplot(DT, 'Color', GRAY)
-hold on
 [invalid, valid] = get_invalid_cells(cell_log_intensity, adj_mat, n);
-scatter(cx(valid), cy(valid), 12, cell_log_intensity(valid), 'filled')
-colorbar
-colormap(jet)
-axis image
-min_white_margin(gca);
-saveas(fig, 'log_intensity', 'png')
 
 disp('Getting initial seeds...')
 % get seeds
@@ -58,28 +51,10 @@ set_size = 20;
 factor = 2;
 k = 100;
 set_size2 = 20;
-[seeds, seeds_rej, seeds_pt, num_s, num_s_pt] = get_seeds_sim_local_max(st_x, en_x, st_y, en_y,...
+[seeds, ~, seeds_pt, num_s, num_s_pt] = get_seeds_sim_local_max(st_x, en_x, st_y, en_y,...
     step_x, step_y, set_size, cell_log_intensity, cell_area, cx, cy, factor, k, set_size2, invalid);
 num = num_s+num_s_pt;
 disp(['Number of regions is ', num2str(num)])
-
-% plot the seeds
-fig = figure;
-triplot(DT, 'Color', GRAY)
-hold on
-% specify the colormap
-colors = lines(num_s);
-for i = 1:num_s
-    scatter(cx(seeds{i}), cy(seeds{i}), 12, colors(i, :), 's', 'filled')
-end
-for i = 1:num_s_pt
-    scatter(cx(seeds_pt{i}), cy(seeds_pt{i}), 12, 'r', 'd', 'filled')
-end
-for i = 1:length(seeds_rej)
-    scatter(cx(seeds_rej{i}), cy(seeds_rej{i}), 12, 'k', '^', 'filled')
-end
-axis image
-saveas(fig, 'seeds', 'png')
 
 seeds_all = [seeds seeds_pt];
 
@@ -88,20 +63,7 @@ region_sets = seeds_all;
 
 disp('Seeded region growing...')
 % graph-based SRG
-[region_sets, labeled_cells] = SRG_graph(region_sets, cell_log_intensity, cell_area, n, adj_mat, invalid', true, 1000);
-
-% plot the over-segmented image
-fig = figure;
-triplot(DT, 'Color', GRAY)
-hold on
-for i = 1:num_s
-    scatter(cx(region_sets{i}), cy(region_sets{i}), 12,  colors(i, :), 'filled')
-end
-for i = 1:num_s_pt
-    scatter(cx(region_sets{i+num_s}), cy(region_sets{i+num_s}), 12, 'r', 'filled')
-end
-axis image
-saveas(fig, 'over_seg', 'png')
+[region_sets, ~] = SRG_graph(region_sets, cell_log_intensity, cell_area, n, adj_mat, invalid', true, 1000);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -109,35 +71,32 @@ disp('Region merging...')
 [sets_all, log_like_all] = merge_region(num, cell_area, ...
     cell_log_intensity, region_sets, adj_mat, n);
 
-%figure
-%plot(log_like_all, '-o')
-
 BIC_all = -2*log_like_all+6*(num-1:-1:0)'*log(n);
-[min_BIC, index_BIC] = min(BIC_all);
+[~, index_BIC] = min(BIC_all);
 
-fig = figure;
-plot(num-1:-1:0, BIC_all, '-o', 'MarkerSize', 3)
-saveas(fig, 'BIC', 'png')
+cx_valid = cx(valid);
+cy_valid = cy(valid);
 
-fig = figure;
-triplot(DT, 'Color', GRAY)
-hold on
-% the final result
 selected = sets_all{index_BIC};
+num = length(selected);
+% Log intensity of all resampled Voronoi cells.
+log_int_all = zeros(length(cx), 1);
 for i = 1:num
     if ~isempty(selected{i})
-        log_int = log(sum(exp(cell_log_intensity(selected{i})).*cell_area(selected{i}))/sum(cell_area(selected{i})));
-        scatter(cx(selected{i}), cy(selected{i}), 12, log_int*ones(length(selected{i}), 1), 'filled')
+        area = sum(cell_area(selected{i}));
+        log_int = log(sum(exp(cell_log_intensity(selected{i})).*cell_area(selected{i}))/area);
+        log_int_all(selected{i}) = log_int;
     end
 end
-colorbar('SouthOutside')
-colormap(hsv)
-axis image
-saveas(fig, 'final_seg', 'png')
 
-% save all variables (exclude figure handle)
-clear fig
-filename = ['real_full_result_', num2str(seed), '.mat'];
-save(filename)
+% Impute log intensity of all original Voronoi cells. 
+impute_log_int = zeros(length(cx_origin), 1);
+for i = 1:length(cx_origin)
+    % Find the nearest (in terms of Euclidean distance) valid resampled
+    % Voronoi cell to impute each original Voronoi cell.
+    dist_vec = pdist2([cx_origin(i), cy_origin(i)], [cx_valid, cy_valid]);
+    [~, index] = min(dist_vec);
+    impute_log_int(i) = log_int_all(valid(index));
+end
 
 end
