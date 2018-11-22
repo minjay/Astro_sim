8d5343b68a1269effd581bfbb515512657eeeadc
minjay <mjfan@ucar.edu>
2018-11-21 20:01:12 -0800

    Add function to fit the model in simulation.

---
+++ sim_fit.m
@@ -0,0 +1,55 @@
+function [labeled_cells, pred_class_all, n_region] = sim_fit(X, show_plot)
+% fit the model in simulation
+
+% init comp
+[cx, cy, n, DT, E, cell_log_intensity, cell_area] = init_comp(X, [0 1], [0 1], ones(size(X, 1), 1));
+adj_mat = get_adj_mat( E, n );
+
+% get seeds
+[invalid, ~] = get_invalid_cells(cell_log_intensity, adj_mat, n);
+[seeds, ~, seeds_pt, num_s, num_s_pt] = get_seeds_sim_local_max(0.1, 0.9, 0.1, 0.9,...
+    0.2, 0.2, 5, cell_log_intensity, cell_area, cx, cy, 2, 50, 5, invalid);
+num = num_s+num_s_pt;
+disp(['Number of regions is ', num2str(num)])
+
+seeds_all = [seeds seeds_pt];
+
+% make a copy of variable seeds
+region_sets = seeds_all;
+
+% graph-based SRG
+[region_sets, labeled_cells] = SRG_graph(region_sets, cell_log_intensity, cell_area, n, adj_mat, invalid');
+
+[sets_all, log_like_all] = merge_region(num, cell_area, ...
+    cell_log_intensity, region_sets, adj_mat, n);
+
+% the factor is 4 instead of 6
+BIC_all = -2*log_like_all+4*(num-1:-1:0)'*log(n);
+[~, index_BIC] = min(BIC_all);
+
+selected = sets_all{index_BIC};
+n_region = 0;
+pred_class_all = zeros(n, 1);
+for i = 1:num
+    if ~isempty(selected{i})
+        n_region = n_region+1;
+        pred_class_all(selected{i}) = n_region;
+    end
+end
+
+if show_plot
+    GRAY = [0.6 0.6 0.6];
+    colors = lines(n_region);
+    triplot(DT, 'Color', GRAY)
+    hold on
+    index = 0;
+    for i = 1:num
+        if ~isempty(selected{i})
+            index = index+1;
+            scatter(cx(selected{i}), cy(selected{i}), 12, colors(index, :), 'filled')
+        end
+    end
+    axis image
+end
+
+end
