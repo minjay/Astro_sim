97416f3d6168b31af5396a49679e86f6949a0151
minjay <mjfan@ucar.edu>
2018-11-21 19:56:55 -0800

    Using other ways to find connected components since graph is not available in old version of Matlab.

--- real_img/get_invalid_cells.m
+++ real_img/get_invalid_cells.m
@@ -5,12 +5,24 @@ function [invalid, valid] = get_invalid_cells(cell_log_intensity, adj_mat, n)
 invalid = find(isnan(cell_log_intensity));
 valid = setdiff(1:n, invalid)';
 
-constr_graph = graph(adj_mat);
-components = conncomp(constr_graph, 'OutputForm', 'cell');
-num_comps = length(components);
+%constr_graph = graph(adj_mat);
+%components = conncomp(constr_graph, 'OutputForm', 'cell');
+%num_comps = length(components);
+%comp_size = zeros(num_comps, 1);
+%for i = 1:num_comps
+%    comp_size(i) = length(components{i});
+%end
+% graph function is not available before R2015b.
+% instead, following 
+% https://blogs.mathworks.com/steve/2007/03/20/connected-component-labeling-part-3/
+adj_mat = adj_mat + eye(size(adj_mat, 1));
+[p, ~, r, ~] = dmperm(adj_mat);
+num_comps = length(r) - 1;
 comp_size = zeros(num_comps, 1);
+components = cell(num_comps, 1);
 for i = 1:num_comps
-    comp_size(i) = length(components{i});
+    comp_size(i) = r(i + 1) - r(i);
+    components{i} = p(r(i):r(i + 1) - 1);
 end
 max_comp_size = max(comp_size);
 for i = 1:num_comps
