3df4d5b14ceaeeb02799ad74f945cdb02d0e8349
minjay <mjfan@ucar.edu>
2018-11-25 11:21:02 -0800

    Plot histograms of number of sources as well.

--- sim/plot_results.m
+++ sim/plot_results.m
@@ -1,21 +1,37 @@
 g1 = [ones(1500, 1); 2*ones(1500, 1); 3*ones(1500, 1)];
 g2 = [ones(500, 3); 2*ones(500, 3); 3*ones(500, 3)];
 g2 = g2(:);
-shift = 0.01;
+reduction = 0.02;
 
+% plot for sim4
 load('sim4_result.mat')
 figure
-boxplot_group(metrics', g1, g2, '(a) Circular extended source')
-min_white_margin(gca, shift)
+boxplot_group(metrics', g1, g2, '')
+xlabel('\alpha')
+min_white_margin(gca, -reduction/2, reduction)
 
+figure
+true_n_source = 5;
+histogram_plot_group(n_region, true_n_source, factors, sample_factors)
+
+% plot for sim5
 load('sim5_result.mat')
 figure
-boxplot_group(metrics', g1, g2, '(b) Z-shaped extended source')
-min_white_margin(gca, shift)
+boxplot_group(metrics', g1, g2, '')
+xlabel('\alpha')
+min_white_margin(gca, -reduction/2, reduction)
 
+figure
+true_n_source = 4;
+histogram_plot_group(n_region, true_n_source, factors, sample_factors)
+
+% plot for sim6
 load('sim6_result.mat')
 figure
-boxplot_group(metrics', g1, g2, '(c) Arc-shaped extended source')
-min_white_margin(gca, shift)
+boxplot_group(metrics', g1, g2, '')
+xlabel('\alpha')
+min_white_margin(gca, -reduction/2, reduction)
 
-
+figure
+true_n_source = 4;
+histogram_plot_group(n_region, true_n_source, factors, sample_factors)
