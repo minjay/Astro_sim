03ae575450c91ed9449a74a270eca1534356cb4c
minjay <mjfan@ucar.edu>
2018-11-25 10:25:44 -0800

    Add optional argument to reduce width of the plot.

--- util/min_white_margin.m
+++ util/min_white_margin.m
@@ -1,7 +1,10 @@
-function [] = min_white_margin(gca, shift)
+function [] = min_white_margin(gca, shift, reduction)
 
 if nargin == 1
     shift = 0;
+    reduction = 0;
+elseif nargin == 2
+    reduction = 0;
 end
 
 ax = gca;
@@ -11,6 +14,6 @@ left = outerpos(1) + ti(1);
 bottom = outerpos(2) + ti(2);
 ax_width = outerpos(3) - ti(1) - ti(3);
 ax_height = outerpos(4) - ti(2) - ti(4);
-ax.Position = [left-shift bottom ax_width ax_height];
+ax.Position = [left-shift bottom ax_width-reduction ax_height];
 
 end
