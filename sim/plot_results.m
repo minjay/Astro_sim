aa10b01c61354312fe3e8641e1da5405653cea63
minjay <mjfan@ucar.edu>
2018-11-24 22:21:59 -0800

    Adjust the plot such that the right end would not be cut.

--- sim/plot_results.m
+++ sim/plot_results.m
@@ -1,20 +1,21 @@
 g1 = [ones(1500, 1); 2*ones(1500, 1); 3*ones(1500, 1)];
 g2 = [ones(500, 3); 2*ones(500, 3); 3*ones(500, 3)];
 g2 = g2(:);
+shift = 0.01;
 
 load('sim4_result.mat')
 figure
 boxplot_group(metrics', g1, g2, '(a) Circular extended source')
-min_white_margin(gca)
+min_white_margin(gca, shift)
 
 load('sim5_result.mat')
 figure
 boxplot_group(metrics', g1, g2, '(b) Z-shaped extended source')
-min_white_margin(gca)
+min_white_margin(gca, shift)
 
 load('sim6_result.mat')
 figure
 boxplot_group(metrics', g1, g2, '(c) Arc-shaped extended source')
-min_white_margin(gca)
+min_white_margin(gca, shift)
 
 
--- util/min_white_margin.m
+++ util/min_white_margin.m
@@ -1,4 +1,8 @@
-function [] = min_white_margin(gca)
+function [] = min_white_margin(gca, shift)
+
+if nargin == 1
+    shift = 0;
+end
 
 ax = gca;
 outerpos = ax.OuterPosition;
@@ -7,6 +11,6 @@ left = outerpos(1) + ti(1);
 bottom = outerpos(2) + ti(2);
 ax_width = outerpos(3) - ti(1) - ti(3);
 ax_height = outerpos(4) - ti(2) - ti(4);
-ax.Position = [left bottom ax_width ax_height];
+ax.Position = [left-shift bottom ax_width ax_height];
 
 end
