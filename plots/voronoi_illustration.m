ecc7c45477fee259c4c78686fb936beda878df2b
minjay <mjfan@ucar.edu>
2018-11-25 16:14:55 -0800

    Improve the color.

--- plots/voronoi_illustration.m
+++ plots/voronoi_illustration.m
@@ -3,23 +3,25 @@ rng(0)
 n = 50;
 X = rand([n, 2]);
 
-fig = figure;
+h = figure;
 cx = X(:, 1);
 cy = X(:, 2);
 DT = delaunayTriangulation(cx, cy);
-% default blue color: http://math.loyola.edu/~loberbro/matlab/html/colorsInMatlab.html#2
-triplot(DT, 'Color', [0, 0.4470, 0.7410], 'LineWidth', 1.2)
+triplot(DT, 'Color', 'k', 'LineWidth', 1.2)
 hold on
 scatter(cx, cy, 48, [0.8500, 0.3250, 0.0980], 'filled')
-title('Delaunay triangulation')
 axis image
-min_white_margin(gca);
+min_white_margin(gca, 0, 0, 0.02);
+set(h, 'Position', [0, 0, 400, 350]);
 
-fig = figure;
+
+h = figure;
 voronoi(cx, cy);
 set(findall(gca, 'Type', 'Line'), 'LineWidth', 1.2);
+set(findall(gca, 'Type', 'Line'), 'Color', 'k');
 hold on
 scatter(cx, cy, 48, [0.8500, 0.3250, 0.0980], 'filled')
-title('Voronoi tessellation')
 axis image
-min_white_margin(gca);
+min_white_margin(gca, 0, 0, 0.02);
+set(h, 'Position', [0, 0, 400, 350]);
+
