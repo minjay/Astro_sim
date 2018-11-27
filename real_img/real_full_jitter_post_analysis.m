6fccf91f2a51b52e974c647a0085340a3dfd4b09
minjay <mjfan@ucar.edu>
2018-11-26 21:57:55 -0800

    Use bar plot instead of histogram plot.

--- real_img/real_full_jitter_post_analysis.m
+++ real_img/real_full_jitter_post_analysis.m
@@ -1,6 +1,8 @@
 load('real_full_result_2018_11_1_23_4_37.mat')
 load('real_full_result_boot.mat')
 
+GRAY = [0.6 0.6 0.6];
+
 n_boot = size(impute_log_int, 2);
 
 boot_std = std(impute_log_int, 0, 2);
@@ -15,7 +17,12 @@ axis image
 min_white_margin(gca);
 saveas(fig, 'log_boot_std', 'png')
 
-histogram(n_region, 10)
+fig = figure;
+unique_n_region = unique(n_region);
+counts = histc(n_region, unique_n_region);
+% use percentage instead of count
+bar(unique_n_region, counts / sum(counts), 'FaceColor', GRAY)
 xlabel('Number of segments')
 ylabel('Frequency')
-min_white_margin(gca);
+set(gca, 'FontSize', 16)
+min_white_margin(gca, 0, 0, 0.01);
