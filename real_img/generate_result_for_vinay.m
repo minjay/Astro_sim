0d00c3a8deccb678722d1b782f61bc5e0f88c9a1
minjay <mjfan@ucar.edu>
2019-03-10 19:41:11 -0700

    Save not only x, y, but also time, PI, cell_area and segment_id.

--- real_img/generate_result_for_vinay.m
+++ real_img/generate_result_for_vinay.m
@@ -1,17 +1,33 @@
 % load fitted result
 load('real_full_result_2018_11_1_23_4_37.mat')
 
+% Comments from Vinay: While playing with segment_id.txt, I realized that 
+% it would be more useful with a couple of extra columns ? one is the 
+% voronoi cell area associated with each photon (so we can get a quick 
+% estimate of the average intensity in each segment), and the time, and 
+% either the energy or PI value that comes with each photon. 
 % read data (in its original scale)
-filename='photon_loc.txt';
+% CIAO command:
+% dmlist "./315/repro/acisf00315_repro_evt2.fits[cols x, y, TIME, PI][sky=region(ds9.reg)]" \
+% data,clean outfile=photons
+filename='photon_loc_time_pi.txt';
 delimiterIn = ' ';
 headerlinesIn = 1;
 A = importdata(filename,delimiterIn,headerlinesIn);
 
 X = A.data;
+n_cols = size(X, 2);
 clear A
 
+% normalization factor
+min_x = min(X(:, 1));
+max_x = max(X(:, 1));
+norm_factor = max_x - min_x;
+cell_area_denorm = cell_area * norm_factor^2;
+X(:, n_cols + 1) = cell_area_denorm; 
+
 % init the last column (recording the segment id)
-X(:, 3) = 0;
+X(:, n_cols + 2) = 0;
 
 num = length(selected);
 area_all = [];
@@ -38,17 +54,21 @@ selected_nonempty = selected_nonempty(sorted_index);
 % to any segments. They are near the edge.
 num_segments = length(selected_nonempty);
 for segment_id = 1:num_segments
-    X(selected_nonempty{segment_id}(1, :), 3) = segment_id;
+    X(selected_nonempty{segment_id}(1, :), n_cols + 2) = segment_id;
 end
 
-save('segment_id.txt', 'X', '-ascii')
+X_table = array2table(X, 'VariableNames', ...
+    {'x' 'y' 'time', 'PI', 'cell_area', 'segment_id'});
+writetable(X_table, 'segment_info.txt', 'Delimiter', ' ')  
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 for segment_id = 1:num_segments
     h = figure;
     scatter(X(:, 1), X(:, 2), '.')
     hold on
-    x_segment = X(X(:, 3) == segment_id, 1);
-    y_segment = X(X(:, 3) == segment_id, 2);
+    x_segment = X(X(:, n_cols + 2) == segment_id, 1);
+    y_segment = X(X(:, n_cols + 2) == segment_id, 2);
     convhull_points = convhull(x_segment, y_segment);
     scatter(x_segment, y_segment, '.')
     plot(x_segment(convhull_points), y_segment(convhull_points), '-')
